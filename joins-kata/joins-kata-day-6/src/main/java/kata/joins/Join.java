0987a9ca35ca05b0d08a99d2df860f6f10127cdf
Alex-Diez <alexvic.amatory@gmail.com>
2016-03-05 14:20:31 +0200

    joins kata sixth day

---
+++ joins-kata/joins-kata-day-6/src/main/java/kata/joins/Join.java
@@ -0,0 +1,33 @@
+package kata.joins;
+
+enum Join {
+    INNER {
+        @Override
+        void keyMatching() {
+        }
+
+        @Override
+        boolean checkMatchingAndReset() {
+            return true;
+        }
+    },
+    OUTER {
+        private boolean match;
+
+        @Override
+        void keyMatching() {
+            match = true;
+        }
+
+        @Override
+        boolean checkMatchingAndReset() {
+            boolean res = match;
+            match = false;
+            return res;
+        }
+    };
+
+    abstract void keyMatching();
+
+    abstract boolean checkMatchingAndReset();
+}
---
+++ joins-kata/joins-kata-day-6/src/main/java/kata/joins/OuterJoin.java
@@ -0,0 +1,13 @@
+package kata.joins;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.List;
+
+public interface OuterJoin {
+
+    <K extends Comparable<K>, V> List<Triple<K, V, V>> leftJoin(List<Pair<K, V>> left, List<Pair<K, V>> right);
+
+    <K extends Comparable<K>, V> List<Triple<K, V, V>> rightJoin(List<Pair<K, V>> left, List<Pair<K, V>> right);
+}
