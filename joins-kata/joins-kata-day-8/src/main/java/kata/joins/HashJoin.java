9ef8402296c43fe64d9a0e52f04407a9b17a8c22
Alex-Diez <alexvic.amatory@gmail.com>
2016-03-07 09:06:07 +0200

    joins kata eighth day

---
+++ joins-kata/joins-kata-day-8/src/main/java/kata/joins/HashJoin.java
@@ -0,0 +1,40 @@
+package kata.joins;
+
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HashJoin
+        implements InnerJoin, OuterJoin {
+
+    private <K extends Comparable<K>, V> List<Triple<K, V, V>> join(List<Pair<K, V>> left, List<Pair<K, V>> right, Join join) {
+        Map<K, V> hash = right.stream().collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+        List<Triple<K, V, V>> collect = new ArrayList<>();
+        for (Pair<K, V> l : left) {
+            if (hash.containsKey(l.getKey())) {
+                join.keyMatching();
+                collect.add(new ImmutableTriple<>(l.getKey(), l.getValue(), hash.get(l.getKey())));
+            }
+            if (!join.checkMatchingAndReset()) {
+                collect.add(new ImmutableTriple<>(l.getKey(), l.getValue(), null));
+            }
+        }
+        return collect;
+    }
+
+    @Override
+    public <K extends Comparable<K>, V> List<Triple<K, V, V>> innerJoin(List<Pair<K, V>> left, List<Pair<K, V>> right) {
+        return join(left, right, Join.INNER);
+    }
+
+    @Override
+    public <K extends Comparable<K>, V> List<Triple<K, V, V>> leftJoin(List<Pair<K, V>> left, List<Pair<K, V>> right) {
+        return join(left, right, Join.OUTER);
+    }
+}
---
+++ joins-kata/joins-kata-day-8/src/test/java/kata/joins/JoinTest.java
@@ -0,0 +1,208 @@
+package kata.joins;
+
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(HierarchicalContextRunner.class)
+public class JoinTest {
+
+    private NestedLoopJoin nestedLoopJoin;
+    private HashJoin hashJoin;
+    private MergeJoin mergeJoin;
+
+    @Before
+    public void setUp() throws Exception {
+        nestedLoopJoin = new NestedLoopJoin();
+        hashJoin = new HashJoin();
+        mergeJoin = new MergeJoin();
+    }
+
+    public class InnerJoin {
+        List<Triple<Integer, Integer, Integer>> result = List.of(
+                new ImmutableTriple<>(1, 1, 1),
+                new ImmutableTriple<>(2, 2, 2),
+                new ImmutableTriple<>(3, 3, 3),
+                new ImmutableTriple<>(4, 4, 4)
+        );
+        List<Pair<Integer, Integer>> view = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(2, 2),
+                new ImmutablePair<>(3, 3),
+                new ImmutablePair<>(4, 4)
+        );
+        List<Triple<Integer, Integer, Integer>> leftRight = List.of(
+                new ImmutableTriple<>(1, 1, 1),
+                new ImmutableTriple<>(2, 2, 2),
+                new ImmutableTriple<>(4, 4, 4)
+        );
+        List<Pair<Integer, Integer>> left = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(2, 2),
+                new ImmutablePair<>(4, 4),
+                new ImmutablePair<>(6, 6)
+        );
+        List<Pair<Integer, Integer>> right = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(2, 2),
+                new ImmutablePair<>(4, 4),
+                new ImmutablePair<>(5, 5)
+        );
+
+        @Test
+        public void itShouldJoinTwoViewWithTheSameDataSetByNestedLoopJoin() throws Exception {
+
+            assertThat(nestedLoopJoin.innerJoin(view, view), is(result));
+        }
+
+        @Test
+        public void itShouldJoinTwoViewWithDifferentDataSetByNestedLoopJoin() throws Exception {
+
+            assertThat(nestedLoopJoin.innerJoin(left, right), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinTwoViewWithTheSameDataSetByHashJoin() throws Exception {
+
+            assertThat(hashJoin.innerJoin(view, view), is(result));
+        }
+
+        @Test
+        public void itShouldJoinTwoViewWithDifferentDataSetByHashJoin() throws Exception {
+
+            assertThat(hashJoin.innerJoin(left, right), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinTwoViewWithTheSameDataSetByMergeJoin() throws Exception {
+
+            assertThat(mergeJoin.innerJoin(view, view), is(result));
+        }
+
+        @Test
+        public void itShouldJoinTwoViewWithDifferentDataSetByMergeJoin() throws Exception {
+
+            assertThat(mergeJoin.innerJoin(left, right), is(leftRight));
+        }
+    }
+
+    public class LeftOuterJoin {
+
+
+        List<Triple<Integer, Integer, Integer>> leftRight = List.of(
+                new ImmutableTriple<>(1, 1, 1),
+                new ImmutableTriple<>(2, 2, null),
+                new ImmutableTriple<>(4, 4, null)
+        );
+        List<Triple<Integer, Integer, Integer>> rightLeft = List.of(
+                new ImmutableTriple<>(1, 1, 1),
+                new ImmutableTriple<>(3, 3, null),
+                new ImmutableTriple<>(5, 5, null)
+        );
+        List<Pair<Integer, Integer>> left = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(2, 2),
+                new ImmutablePair<>(4, 4)
+        );
+        List<Pair<Integer, Integer>> right = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(3, 3),
+                new ImmutablePair<>(5, 5)
+        );
+
+        @Test
+        public void itShouldJoinLeftWithRightByNestedLoopJoin() throws Exception {
+            assertThat(nestedLoopJoin.leftJoin(left, right), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinRightWithLeftByNestedLoopJoin() throws Exception {
+            assertThat(nestedLoopJoin.leftJoin(right, left), is(rightLeft));
+        }
+
+        @Test
+        public void itShouldJoinLeftWithRightByHashJoin() throws Exception {
+            assertThat(hashJoin.leftJoin(left, right), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinRightWithLeftByHashJoin() throws Exception {
+            assertThat(hashJoin.leftJoin(right, left), is(rightLeft));
+        }
+
+        @Test
+        public void itShouldJoinLeftWithRightByMergeJoin() throws Exception {
+            assertThat(mergeJoin.leftJoin(left, right), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinRightWithLeftByMergeJoin() throws Exception {
+            assertThat(mergeJoin.leftJoin(right, left), is(rightLeft));
+        }
+    }
+
+    public class RightOuterJoin {
+        List<Triple<Integer, Integer, Integer>> rightLeft = List.of(
+                new ImmutableTriple<>(1, 1, 1),
+                new ImmutableTriple<>(2, 2, null),
+                new ImmutableTriple<>(4, 4, null)
+        );
+        List<Triple<Integer, Integer, Integer>> leftRight = List.of(
+                new ImmutableTriple<>(1, 1, 1),
+                new ImmutableTriple<>(3, 3, null),
+                new ImmutableTriple<>(5, 5, null)
+        );
+        List<Pair<Integer, Integer>> right = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(2, 2),
+                new ImmutablePair<>(4, 4)
+        );
+        List<Pair<Integer, Integer>> left = List.of(
+                new ImmutablePair<>(1, 1),
+                new ImmutablePair<>(3, 3),
+                new ImmutablePair<>(5, 5)
+        );
+
+        @Test
+        public void itShouldJoinRightWithLeftByNestedLoopJoin() throws Exception {
+            assertThat(nestedLoopJoin.rightJoin(left, right), is(rightLeft));
+        }
+
+        @Test
+        public void itShouldJoinLeftWithRightByNestedLoopJoin() throws Exception {
+            assertThat(nestedLoopJoin.rightJoin(right, left), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinRightWithLeftByHashJoin() throws Exception {
+            assertThat(hashJoin.rightJoin(left, right), is(rightLeft));
+        }
+
+        @Test
+        public void itShouldJoinLeftWithRightByHashJoin() throws Exception {
+            assertThat(hashJoin.rightJoin(right, left), is(leftRight));
+        }
+
+        @Test
+        public void itShouldJoinRightWithLeftByMergeJoin() throws Exception {
+            assertThat(mergeJoin.rightJoin(left, right), is(rightLeft));
+        }
+
+        @Test
+        public void itShouldJoinLeftWithRightByMergeJoin() throws Exception {
+            assertThat(mergeJoin.rightJoin(right, left), is(leftRight));
+        }
+    }
+}
