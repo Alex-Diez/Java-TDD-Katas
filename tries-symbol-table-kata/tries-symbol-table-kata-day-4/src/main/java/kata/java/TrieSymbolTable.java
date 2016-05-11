b49f68b364d9b778cdf714a9e843d620f450721b
Alex-Diez <alexvic.amatory@gmail.com>
2016-05-11 09:07:43 +0300

    tries symbol table kata fourth day

---
+++ tries-symbol-table-kata/tries-symbol-table-kata-day-4/src/main/java/kata/java/TrieSymbolTable.java
@@ -0,0 +1,40 @@
+package kata.java;
+
+import it.unimi.dsi.fastutil.chars.Char2ObjectMap;
+import it.unimi.dsi.fastutil.chars.Char2ObjectOpenHashMap;
+
+import java.util.Optional;
+
+public class TrieSymbolTable<V> {
+
+    private final Node root;
+
+    public TrieSymbolTable() {
+        root = new Node();
+    }
+
+    public Optional<V> get(String key) {
+        Node node = root;
+        for (int i = 0; i < key.length() && node != null; i++) {
+            node = node.next.get(key.charAt(i));
+        }
+        return node != null ? Optional.ofNullable(node.value) : Optional.empty();
+    }
+
+    public void put(String key, V value) {
+        Node node = root;
+        for (int i = 0; i < key.length(); i++) {
+            node = node.next.computeIfAbsent(key.charAt(i), k -> new Node());
+        }
+        node.value = value;
+    }
+
+    private class Node {
+        private V value;
+        private final Char2ObjectMap<Node> next;
+
+        private Node() {
+            this.next = new Char2ObjectOpenHashMap<>();
+        }
+    }
+}
