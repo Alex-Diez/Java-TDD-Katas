2e96e1a68b17cc61c2e6f626df913a574cda2d00
Alex-Diez <alexvic.amatory@gmail.com>
2016-05-14 09:09:50 +0300

    tries symbol table kata seventh day

---
+++ tries-symbol-table-kata/tries-symbol-table-kata-day-7/src/test/java/kata/java/TrieSymbolTableTest.java
@@ -0,0 +1,50 @@
+package kata.java;
+
+import co.unruly.matchers.OptionalMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TrieSymbolTableTest {
+
+    private TrieSymbolTable<Integer> table;
+
+    @Before
+    public void setUp() throws Exception {
+        table = new TrieSymbolTable<>();
+    }
+
+    @Test
+    public void itShouldNotContain_whenValueIsNotPutIn() throws Exception {
+        assertThat(table.get("key"), OptionalMatchers.empty());
+    }
+
+    @Test
+    public void itShouldContain_whenValueIsPutIn() throws Exception {
+        table.put("key", 1);
+
+        assertThat(table.get("key"), OptionalMatchers.contains(1));
+    }
+
+    @Test
+    public void itShouldOverrideOldValueByNew() throws Exception {
+        table.put("key", 1);
+        table.put("key", 2);
+
+        assertThat(table.get("key"), OptionalMatchers.contains(2));
+    }
+
+    @Test
+    public void itShouldContainMultipleKeyValuePairs() throws Exception {
+        table.put("key-1", 1);
+        table.put("key-2", 2);
+        table.put("key-3", 3);
+        table.put("key-4", 4);
+
+        assertThat(table.get("key-1"), OptionalMatchers.contains(1));
+        assertThat(table.get("key-2"), OptionalMatchers.contains(2));
+        assertThat(table.get("key-3"), OptionalMatchers.contains(3));
+        assertThat(table.get("key-4"), OptionalMatchers.contains(4));
+    }
+}
