58b7d720674c8801a203d6dc608cc808df06dba5
Alex-Diez <alexvic.amatory@gmail.com>
2016-06-02 08:57:01 +0300

    bowling kata second day

---
+++ bowling-kata/bowling-kata-day-2/src/main/java/kata/java/Game.java
@@ -0,0 +1,47 @@
+package kata.java;
+
+public class Game {
+    private int[] rolls = new int[20];
+    private int size = 0;
+
+    public void roll(int pins) {
+        rolls[size] = pins;
+        size += 1;
+    }
+
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rolls[frameIndex])) {
+                score += strikePoints(frameIndex);
+                frameIndex += 1;
+            }
+            else if (isSpare(frameIndex)) {
+                score += sparePoints(frameIndex);
+                frameIndex += 2;
+            }
+            else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private int sparePoints(int frameIndex) {
+        return 10 + rolls[frameIndex + 2];
+    }
+
+    private int strikePoints(int frameIndex) {
+        return 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+}
