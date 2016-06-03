49398f6e6a05a842d27994be88468bda7da23945
Alex-Diez <alexvic.amatory@gmail.com>
2016-06-03 11:31:21 +0300

    bowling kata third day

---
+++ bowling-kata/bowling-kata-day-3/src/test/java/kata/java/GameTest.java
@@ -0,0 +1,62 @@
+package kata.java;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class GameTest {
+
+    private Game game;
+
+    private void rollMany(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
+    @Test
+    public void gutterGame() throws Exception {
+        rollMany(20, 0);
+
+        assertThat(game.score(), is(0));
+    }
+
+    @Test
+    public void allOnes() throws Exception {
+        rollMany(20, 1);
+
+        assertThat(game.score(), is(20));
+    }
+
+    @Test
+    public void oneSpare() throws Exception {
+        rollSpare();
+        game.roll(3);
+        rollMany(17, 0);
+
+        assertThat(game.score(), is(16));
+    }
+
+    @Test
+    public void oneStrike() throws Exception {
+        game.roll(10);
+        game.roll(4);
+        game.roll(3);
+        rollMany(16, 0);
+
+        assertThat(game.score(), is(24));
+    }
+}
