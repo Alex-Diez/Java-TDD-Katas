8f77ef10d7be962f609946bcaff65436b6d396a3
Alex-Diez <alex.dukhno@icloud.com>
2017-03-15 19:24:46 +0200

    probe map kata eighth day

---
+++ linear-probe-map-kata/probe-map-day-8/src/main/java/kata/java/ProbeMap.java
@@ -0,0 +1,108 @@
+package kata.java;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class ProbeMap {
+    private static final int MIN_CAPACITY = 16;
+    private static final int MAX_CAPACITY = Integer.MIN_VALUE >>> 1;
+
+    private static int alignCapacity(int initCapacity) {
+        int n = initCapacity - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return n < MIN_CAPACITY ? MIN_CAPACITY : n >= MAX_CAPACITY ? MAX_CAPACITY : n + 1;
+    }
+
+    private final int missedValue;
+    private int[] values;
+    private int[] keys;
+    private int capacity;
+    private int size;
+
+    public ProbeMap() {
+        this(MIN_CAPACITY);
+    }
+
+    public ProbeMap(int initCapacity) {
+        this(initCapacity, Integer.MIN_VALUE);
+    }
+
+    public ProbeMap(int initCapacity, int missedValue) {
+        this.missedValue = missedValue;
+        capacity = alignCapacity(initCapacity);
+        values = new int[capacity];
+        Arrays.fill(values, missedValue);
+        keys = new int[capacity];
+        Arrays.fill(keys, missedValue);
+        size = 0;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
+    public void put(int key, int value) {
+        int index = key & capacity - 1;
+        if (keys[index] == key) {
+            values[index] = value;
+        } else {
+            while (values[index] != missedValue) {
+                index = index + 1 & capacity - 1;
+            }
+            values[index] = value;
+            keys[index] = key;
+            size += 1;
+            if (size == capacity - 1) {
+                int newCapacity = capacity << 1;
+                int[] newKeys = new int[newCapacity];
+                Arrays.fill(newKeys, missedValue);
+                int[] newValues = new int[newCapacity];
+                Arrays.fill(newValues, missedValue);
+
+                for (int i = 0; i < capacity; i++) {
+                    if (keys[i] != missedValue) {
+                        int newIndex = keys[i] & newCapacity - 1;
+                        while (newValues[newIndex] != missedValue) {
+                            newIndex = newIndex + 1 & newCapacity - 1;
+                        }
+                        newKeys[newIndex] = keys[i];
+                        newValues[newIndex] = values[i];
+                    }
+                }
+
+                capacity = newCapacity;
+                keys = newKeys;
+                values = newValues;
+            }
+        }
+    }
+
+    public OptionalInt get(int key) {
+        int index = key & capacity - 1;
+        if (values[index] == missedValue) {
+            return OptionalInt.empty();
+        } else {
+            while (keys[index] != key) {
+                index = index + 1 & capacity - 1;
+            }
+            return OptionalInt.of(values[index]);
+        }
+    }
+
+    public int missedValue() {
+        return missedValue;
+    }
+
+    public Iterable<Integer> keys() {
+        return () -> IntStream.of(keys).filter(key -> key != missedValue).iterator();
+    }
+
+    public Iterable<Integer> values() {
+        return () -> IntStream.of(values).filter(value -> value != missedValue).iterator();
+    }
+}
---
+++ linear-probe-map-kata/probe-map-day-8/src/test/java/kata/java/ProbeMapTest.java
@@ -0,0 +1,141 @@
+package kata.java;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
+
+public class ProbeMapTest {
+
+    @Test
+    public void createsMapWithDefaultCapacity() throws Exception {
+        assertThat(new ProbeMap().capacity(), is(16));
+    }
+    
+    @Test
+    public void createsMapWithCapacityLesserThanDefault() throws Exception {
+        assertThat(new ProbeMap(10).capacity(), is(16));
+    }
+
+    @Test
+    public void createsMapWithCapacityGreaterThanDefault() throws Exception {
+        assertThat(new ProbeMap(20).capacity(), is(32));
+    }
+
+    @Test
+    public void putOneKey() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        map.put(1, 1);
+
+        assertThat(map.get(1), is(OptionalInt.of(1)));
+        assertThat(map.get(1), is(OptionalInt.of(1)));
+    }
+
+    @Test
+    public void putTheSameKeyTwice() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        map.put(1, 1);
+        map.put(1, 2);
+
+        assertThat(map.get(1), is(OptionalInt.of(2)));
+    }
+
+    @Test
+    public void createsMapWithDefaultMissedValue() throws Exception {
+        assertThat(new ProbeMap().missedValue(), is(Integer.MIN_VALUE));
+    }
+
+    @Test
+    public void createsMapWithSpecifiedMissedValue() throws Exception {
+        assertThat(new ProbeMap(16, Integer.MAX_VALUE).missedValue(), is(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void getKeyThatDoesNotExist() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        assertThat(map.get(1), is(OptionalInt.empty()));
+    }
+
+    @Test
+    public void putManyKeys() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        map.put(1, 1);
+        map.put(2, 2);
+        map.put(3, 3);
+
+        assertThat(map.get(1), is(OptionalInt.of(1)));
+        assertThat(map.get(2), is(OptionalInt.of(2)));
+        assertThat(map.get(3), is(OptionalInt.of(3)));
+    }
+
+    @Test
+    public void putBigKey() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        map.put(1000, 1000);
+
+        assertThat(map.get(1000), is(OptionalInt.of(1000)));
+    }
+
+    @Test
+    public void putKeysWithCollision() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        map.put(0, 0);
+        map.put(16, 16);
+        map.put(32, 32);
+
+        assertThat(map.get(0), is(OptionalInt.of(0)));
+        assertThat(map.get(16), is(OptionalInt.of(16)));
+        assertThat(map.get(32), is(OptionalInt.of(32)));
+    }
+
+    @Test
+    public void mapKeys() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        IntStream.range(0, 10).forEach(key -> map.put(key, key + 20));
+
+        assertThat(map.keys(), containsInAnyOrder(IntStream.range(0, 10).boxed().toArray()));
+    }
+
+    @Test
+    public void mapValues() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        IntStream.range(0, 10).forEach(value -> map.put(value, value));
+
+        assertThat(map.values(), containsInAnyOrder(IntStream.range(0, 10).boxed().toArray()));
+    }
+
+    @Test
+    public void putKeysMoreThanCapacity() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        IntStream.range(0, 20).forEach(item -> map.put(item, item + 20));
+
+        assertThat(map.capacity(), is(32));
+        assertThat(map.keys(), containsInAnyOrder(IntStream.range(0, 20).boxed().toArray()));
+        assertThat(map.values(), containsInAnyOrder(IntStream.range(20, 40).boxed().toArray()));
+    }
+
+    @Test
+    public void putCKeysWithCollisionsMoreThanCapacity() throws Exception {
+        ProbeMap map = new ProbeMap();
+
+        IntStream.iterate(0, item -> item + 16).limit(20).forEach(key -> map.put(key, key + 20));
+
+        assertThat(map.capacity(), is(32));
+        assertThat(map.keys(), containsInAnyOrder(IntStream.iterate(0, item -> item + 16).limit(20).boxed().toArray()));
+        assertThat(map.values(), containsInAnyOrder(IntStream.iterate(20, item -> item + 16).limit(20).boxed().toArray()));
+    }
+}
