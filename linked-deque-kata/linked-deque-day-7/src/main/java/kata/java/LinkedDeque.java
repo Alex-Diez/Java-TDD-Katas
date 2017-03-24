3c9a7ca248f6f1f09d059571f5436a322ee1c259
Alex-Diez <alex.dukhno@icloud.com>
2017-03-24 17:24:32 +0200

    linked deque kata seventh day

---
+++ linked-deque-kata/linked-deque-day-7/src/main/java/kata/java/LinkedDeque.java
@@ -0,0 +1,90 @@
+package kata.java;
+
+import java.util.Iterator;
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+
+public class LinkedDeque implements Iterable<Integer> {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void addFront(int item) {
+        Node newHead = new Node(item);
+        newHead.next = head;
+        updateFront(newHead);
+        size += 1;
+        head = newHead;
+    }
+
+    public OptionalInt removeFront() {
+        if (head == null) {
+            return OptionalInt.empty();
+        }
+        Node oldHead = head;
+        head = head.next;
+        updateFront(null);
+        oldHead.next = null;
+        size -= 1;
+        return OptionalInt.of(oldHead.item);
+    }
+
+    private void updateFront(Node node) {
+        if (head != null) {
+            head.prev = node;
+        } else {
+            tail = node;
+        }
+    }
+
+    public void addBack(int item) {
+        Node newTail = new Node(item);
+        newTail.prev = tail;
+        updateBack(newTail);
+        size += 1;
+        tail = newTail;
+    }
+
+    public OptionalInt removeBack() {
+        if (tail == null) {
+            return OptionalInt.empty();
+        }
+        Node oldTail = tail;
+        tail = tail.prev;
+        updateBack(null);
+        oldTail.prev = null;
+        size -= 1;
+        return OptionalInt.of(oldTail.item);
+    }
+
+    private void updateBack(Node node) {
+        if (tail != null) {
+            tail.next = node;
+        } else {
+            head = node;
+        }
+    }
+
+    public OptionalInt peekFront() {
+        return head == null ? OptionalInt.empty() : OptionalInt.of(head.item);
+    }
+
+    public OptionalInt peekBack() {
+        return tail == null ? OptionalInt.empty() : OptionalInt.of(tail.item);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return Stream.iterate(head, node -> node.next).limit(size).mapToInt(node -> node.item).iterator();
+    }
+
+    private static class Node {
+        private final int item;
+        private Node next;
+        private Node prev;
+
+        Node(int item) {
+            this.item = item;
+        }
+    }
+}
