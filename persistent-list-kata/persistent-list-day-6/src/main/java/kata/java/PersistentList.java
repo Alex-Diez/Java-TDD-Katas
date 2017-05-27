254afc4ed6aab67d839244e50b2ceb3bbae2f0ce
Alex-Diez <alex.dukhno@icloud.com>
2017-05-27 17:20:53 +0300

    persistent list kata sixth day

---
+++ persistent-list-kata/persistent-list-day-6/src/main/java/kata/java/PersistentList.java
@@ -0,0 +1,80 @@
+package kata.java;
+
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class PersistentList<E> {
+    private static final PersistentList<?> EMPTY = new PersistentList<>(null, null);
+    private final E head;
+    private final PersistentList<E> tail;
+
+    private PersistentList(E head, PersistentList<E> tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public static <E> PersistentList<E> empty() {
+        return (PersistentList<E>)EMPTY;
+    }
+
+    @Override
+    public String toString() {
+        PersistentList<E> list = this;
+        StringBuilder builder = new StringBuilder();
+        builder.append('[');
+        if (list != EMPTY) {
+            while (list.tail != EMPTY) {
+                builder.append(list.head);
+                builder.append(", ");
+                list = list.tail;
+            }
+            builder.append(list.head);
+        }
+        builder.append(']');
+        return builder.toString();
+    }
+
+    public PersistentList<E> prepend(E item) {
+        return new PersistentList<>(item, this);
+    }
+
+    public Optional<E> head() {
+        return Optional.ofNullable(head);
+    }
+
+    public PersistentList<E> tail() {
+        return this == EMPTY ? empty() : tail;
+    }
+
+    public PersistentList<E> drop(int n) {
+        if (this == EMPTY) return empty();
+        else if (n < 1) return this;
+        else return tail.drop(n - 1);
+    }
+
+    public PersistentList<E> dropWhile(Predicate<E> predicate) {
+        if (this == EMPTY) return empty();
+        else if (!predicate.test(head)) return this;
+        else return tail.dropWhile(predicate);
+    }
+
+    public PersistentList<E> filter(Predicate<E> predicate) {
+        if (this == EMPTY) return empty();
+        else if (!predicate.test(head)) return tail.filter(predicate);
+        else return new PersistentList<>(head, tail.filter(predicate));
+    }
+
+    public PersistentList<E> reverse() {
+        return foldLeft((item, acc) -> acc.prepend(item), empty());
+    }
+
+    public <R> PersistentList<R> map(Function<E, R> map) {
+        return reverse().foldLeft((item, acc) -> acc.prepend(map.apply(item)), empty());
+    }
+
+    public <A> A foldLeft(BiFunction<E, A, A> function, A accumulator) {
+        return this == EMPTY ? accumulator : tail.foldLeft(function, function.apply(head, accumulator));
+    }
+}
---
+++ persistent-list-kata/persistent-list-day-6/src/test/java/kata/java/PersistentListTest.java
@@ -0,0 +1,122 @@
+package kata.java;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PersistentListTest {
+
+    private PersistentList<Integer> empty;
+    private PersistentList<Integer> nonempty;
+
+    @Before
+    public void setUp() throws Exception {
+        empty = PersistentList.empty();
+        nonempty = empty.prepend(10).prepend(20).prepend(30);
+    }
+
+    @Test
+    public void createEmptyList() throws Exception {
+        assertThat(empty.toString(), is("[]"));
+    }
+
+    @Test
+    public void prependItemToList() throws Exception {
+        assertThat(empty.prepend(10).toString(), is("[10]"));
+    }
+
+    @Test
+    public void prependManyItemsToList() throws Exception {
+        assertThat(nonempty.toString(), is("[30, 20, 10]"));
+    }
+
+    @Test
+    public void headOfEmptyListIsEmptyOption() throws Exception {
+        assertThat(empty.head(), is(Optional.empty()));
+    }
+    
+    @Test
+    public void headOfNonemptyListIsTheFirstItem() throws Exception {
+        assertThat(nonempty.head(), is(Optional.of(30)));
+    }
+
+    @Test
+    public void tailOfEmptyListIsEmptyList() throws Exception {
+        assertThat(empty.tail().toString(), is("[]"));
+    }
+    
+    @Test
+    public void tailOfNonemptyListIsTheAllItemsExceptTheFirstOne() throws Exception {
+        assertThat(nonempty.tail().toString(), is("[20, 10]"));
+    }
+
+    @Test
+    public void dropsNothingFromEmptyList() throws Exception {
+        assertThat(empty.drop(10).toString(), is("[]"));
+    }
+
+    @Test
+    public void dropsNothingFromNonemptyList() throws Exception {
+        assertThat(nonempty.drop(-1).toString(), is("[30, 20, 10]"));
+    }
+
+    @Test
+    public void dropsTwoItemsFromNonemptyList() throws Exception {
+        assertThat(nonempty.drop(2).toString(), is("[10]"));
+    }
+
+    @Test
+    public void dropsNothingFromEmptyListByPredicate() throws Exception {
+        assertThat(empty.dropWhile(i -> i > 10).toString(), is("[]"));
+    }
+
+    @Test
+    public void dropsNothingFromNonemptyListByPredicate() throws Exception {
+        assertThat(nonempty.dropWhile(i -> i > 50).toString(), is("[30, 20, 10]"));
+    }
+
+    @Test
+    public void dropsTwoItemsFromNonemptyListByPredicate() throws Exception {
+        assertThat(nonempty.dropWhile(i -> i > 10).toString(), is("[10]"));
+    }
+
+    @Test
+    public void filtersOutNothingFromEmptyList() throws Exception {
+        assertThat(empty.filter(i -> i == 10).toString(), is("[]"));
+    }
+    
+    @Test
+    public void filtersOutNothingFromNonemptyList() throws Exception {
+        assertThat(nonempty.filter(i -> i > 0).toString(), is("[30, 20, 10]"));
+    }
+
+    @Test
+    public void filtersOutTwoItemsFromNonemptyList() throws Exception {
+        assertThat(nonempty.filter(i -> i == 20).toString(), is("[20]"));
+    }
+
+    @Test
+    public void emptyListReversedIntoItself() throws Exception {
+        assertThat(empty.reverse().toString(), is("[]"));
+    }
+
+    @Test
+    public void singleListReversedIntoItself() throws Exception {
+        PersistentList single = PersistentList.empty().prepend(10);
+        assertThat(single.reverse().toString(), is("[10]"));
+    }
+    
+    @Test
+    public void reversedListContainsItemsInReversedOrder() throws Exception {
+        assertThat(nonempty.reverse().toString(), is("[10, 20, 30]"));
+    }
+
+    @Test
+    public void mapContentToDouble() throws Exception {
+        assertThat(nonempty.map(Integer::doubleValue).toString(), is("[30.0, 20.0, 10.0]"));
+    }
+}
