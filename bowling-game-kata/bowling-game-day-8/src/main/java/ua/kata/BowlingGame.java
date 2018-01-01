7f4db480a7a4d33b511e23b1262c69294d60890d
Alex-Diez <alex.dukhno@icloud.com>
2018-01-01 20:32:08 +0200

    bowling game kata eighth day

---
+++ bowling-game-kata/bowling-game-day-8/src/main/java/ua/kata/BowlingGame.java
@@ -0,0 +1,46 @@
+package ua.kata;
+
+import java.util.stream.IntStream;
+
+public class BowlingGame {
+  private int[] rolls = new int[21];
+  private int current;
+
+  public void roll(int pin) {
+    rolls[current++] = pin;
+  }
+
+  public int score() {
+    return IntStream.iterate(0, this::generateNextFrameIndex).limit(10).map(this::computeFramePoints).sum();
+  }
+
+  private int computeFramePoints(int frameIndex) {
+    if (isStrike(frameIndex)) {
+      return 10 + strikeBonus(frameIndex);
+    } else if (isSpare(frameIndex)) {
+      return 10 + spareBonus(frameIndex);
+    } else {
+      return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+  }
+
+  private int generateNextFrameIndex(int frameIndex) {
+    return isStrike(frameIndex) ? frameIndex + 1 : frameIndex + 2;
+  }
+
+  private boolean isStrike(int frameIndex) {
+    return rolls[frameIndex] == 10;
+  }
+
+  private int strikeBonus(int frameIndex) {
+    return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+  }
+
+  private boolean isSpare(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+  }
+
+  private int spareBonus(int frameIndex) {
+    return rolls[frameIndex + 2];
+  }
+}
