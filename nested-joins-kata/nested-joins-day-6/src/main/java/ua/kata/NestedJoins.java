7ab8c5179e4bae149a79c5b0c2b03cd4c86bb289
Alex-Diez <alex.dukhno@icloud.com>
2018-01-30 11:43:32 +0200

    nested joins kata sixth day

---
+++ nested-joins-kata/nested-joins-day-6/src/main/java/ua/kata/NestedJoins.java
@@ -0,0 +1,39 @@
+package ua.kata;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.function.BiFunction;
+
+import reactor.core.publisher.Flux;
+
+public class NestedJoins<K extends Comparable<K>, V> {
+  public Flux<Triple<K, V, V>> joinLeft(Flux<Pair<K, V>> left, Flux<Pair<K, V>> right) {
+    return nestedJoin(left, right, (p, s) -> new ImmutableTriple<>(p.getKey(), p.getValue(), s.getValue()));
+  }
+
+  public Flux<Triple<K, V, V>> joinRight(Flux<Pair<K, V>> left, Flux<Pair<K, V>> right) {
+    return nestedJoin(right, left, (p, s) -> new ImmutableTriple<>(p.getKey(), s.getValue(), p.getValue()));
+  }
+
+  private Flux<Triple<K, V, V>> nestedJoin(
+      Flux<Pair<K, V>> primary,
+      Flux<Pair<K, V>> secondary,
+      BiFunction<Pair<K, V>, Pair<K, V>, Triple<K, V, V>> compose) {
+    return primary.flatMap(
+        primaryPair -> secondary.filter(secondaryPair -> equalByKeys(primaryPair, secondaryPair))
+            .defaultIfEmpty(nullifiedValue(primaryPair))
+            .map(secondaryPair -> compose.apply(primaryPair, secondaryPair))
+    );
+  }
+
+  private boolean equalByKeys(Pair<K, V> primaryPair, Pair<K, V> secondaryPair) {
+    return primaryPair.getKey().compareTo(secondaryPair.getKey()) == 0;
+  }
+
+  private ImmutablePair<K, V> nullifiedValue(Pair<K, V> primaryPair) {
+    return new ImmutablePair<>(primaryPair.getKey(), null);
+  }
+}
