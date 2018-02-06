a7677b2f81c00f9025c5aa6dea7e87737201e99d
Alex-Diez <alex.dukhno@icloud.com>
2018-02-06 21:24:13 +0200

    nested joins kata thirteenth day

---
+++ nested-joins-kata/nested-joins-day-13/src/main/java/ua/kata/NestedJoins.java
@@ -0,0 +1,32 @@
+package ua.kata;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.function.BiFunction;
+
+import reactor.core.publisher.Flux;
+
+public class NestedJoins<K extends Comparable<K>, V> {
+  public Flux<Triple<K, V, V>> leftJoin(Flux<Pair<K, V>> left, Flux<Pair<K, V>> right) {
+    return nestedJoin(left, right, (p, s) -> new ImmutableTriple<>(p.getKey(), p.getValue(), s.getValue()));
+  }
+
+  public Flux<Triple<K, V, V>> rightJoin(Flux<Pair<K, V>> left, Flux<Pair<K, V>> right) {
+    return nestedJoin(right, left, (p, s) -> new ImmutableTriple<>(p.getKey(), s.getValue(), p.getValue()));
+  }
+
+  private Flux<Triple<K, V, V>> nestedJoin(
+      Flux<Pair<K, V>> primary,
+      Flux<Pair<K, V>> secondary,
+      BiFunction<Pair<K, V>, Pair<K, V>, Triple<K, V, V>> composeRule) {
+    return primary.flatMap(
+        primaryPair ->
+            secondary.filter(secondaryPair -> primaryPair.getKey().compareTo(secondaryPair.getKey()) == 0)
+                .defaultIfEmpty(new ImmutablePair<>(primaryPair.getKey(), null))
+                .map(secondaryPair -> composeRule.apply(primaryPair, secondaryPair))
+    );
+  }
+}
