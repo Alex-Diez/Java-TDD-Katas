6ba81a8099a49e90c6a8d6af3f62972647d2f889
Alex-Diez <alex.dukhno@icloud.com>
2018-03-05 09:57:23 +0200

    string calc kata fourteenth day

---
+++ string-calc-kata/string-calc-day-14/src/main/java/ua/kata/StringCalc.java
@@ -0,0 +1,87 @@
+package ua.kata;
+
+import java.util.Map;
+import java.util.function.IntBinaryOperator;
+
+public class StringCalc {
+
+  public int compute(String src) {
+    return new Parser(src).parseExpression();
+  }
+
+  private static class Parser {
+    private final Map<Character, IntBinaryOperator> highOrderOperators = Map.ofEntries(
+        Map.entry('*', (a, b) -> a * b),
+        Map.entry('/', (a, b) -> a / b)
+    );
+    private final Map<Character, IntBinaryOperator> lowOrderOperations = Map.ofEntries(
+        Map.entry('+', (a, b) -> a + b),
+        Map.entry('-', (a, b) -> a - b)
+    );
+
+    private final String src;
+    private int index;
+
+    Parser(String src) {
+      this.src = src;
+    }
+
+    private int parseExpression() {
+      int num = parseTerm();
+      IntBinaryOperator operator;
+      while ((operator = lowOrderOperations.get(parseOperation())) != null) {
+        skip();
+        num = operator.applyAsInt(num, parseTerm());
+      }
+      return num;
+    }
+
+    int parseTerm() {
+      int num = parseArgument();
+      IntBinaryOperator operator;
+      while ((operator = highOrderOperators.get(parseOperation())) != null) {
+        skip();
+        num = operator.applyAsInt(num, parseArgument());
+      }
+      return num;
+    }
+
+    int parseArgument() {
+      if (isSubexpression()) {
+        return parseSubexpression();
+      } else {
+        return parseNumber();
+      }
+    }
+
+    private boolean isSubexpression() {
+      return src.charAt(index) == '(';
+    }
+
+    private int parseSubexpression() {
+      skip();
+      int ret = parseExpression();
+      skip();
+      return ret;
+    }
+
+    private int parseNumber() {
+      int start = index;
+      int end = lastIndex() + (int) src.chars().skip(lastIndex()).takeWhile(Character::isDigit).count();
+      index = end;
+      return Integer.parseInt(src.substring(start, end));
+    }
+
+    private int lastIndex() {
+      return src.charAt(index) == '-' ? index + 1 : index;
+    }
+
+    char parseOperation() {
+      return index >= src.length() ? 0 : src.charAt(index);
+    }
+
+    void skip() {
+      index++;
+    }
+  }
+}
