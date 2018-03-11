e6dcfb0da81d6c0dff6b8fe6daf8e064c4086908
Alex-Diez <alex.dukhno@icloud.com>
2018-03-11 12:46:20 +0200

    persistent queue kata third day

--- persistent-queue-kata/persistent-queue-day-3/src/main/java/ua/kata/PersistentQueue.java
+++ persistent-queue-kata/persistent-queue-day-3/src/main/java/ua/kata/PersistentQueue.java
@@ -4,14 +4,64 @@ import java.util.Optional;
 
 public class PersistentQueue {
 
+  private final Node head;
+  private final Node tail;
+
+  public PersistentQueue() {
+    this(null, null);
+  }
+
+  private PersistentQueue(Node head, Node tail) {
+    this.head = head;
+    this.tail = tail;
+  }
+
   public Tuple dequeue() {
-    return new Tuple();
+    return Optional.ofNullable(head).map(Tuple::new).orElse(new Tuple(null));
+  }
+
+  public PersistentQueue enqueue(Integer value) {
+    return Optional.ofNullable(head)
+        .map(h -> new PersistentQueue(h, new Node(value, this)))
+        .orElseGet(() -> new PersistentQueue(new Node(value, this), new Node(value, this)));
+  }
+
+  @Override
+  public String toString() {
+    return "Queue {" +
+        "head: " + head +
+        ", tail:" + tail +
+        '}';
+  }
+
+  private static class Node {
+    final Integer value;
+    final PersistentQueue prev;
+
+    Node(Integer value, PersistentQueue prev) {
+      this.value = value;
+      this.prev = prev;
+    }
+
+    @Override
+    public String toString() {
+      return "Node[" + value + ']';
+    }
   }
 
   public static class Tuple {
+    private final Node value;
+
+    Tuple(Node value) {
+      this.value = value;
+    }
+
+    public Optional<PersistentQueue> queue() {
+      return Optional.ofNullable(value).map(node -> node.prev);
+    }
 
     public Optional<Integer> value() {
-      return Optional.empty();
+      return Optional.ofNullable(value).map(n -> n.value);
     }
   }
 }
--- persistent-queue-kata/persistent-queue-day-3/src/test/java/ua/kata/PersistentQueueTest.java
+++ persistent-queue-kata/persistent-queue-day-3/src/test/java/ua/kata/PersistentQueueTest.java
@@ -1,17 +1,64 @@
 package ua.kata;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static ua.kata.PersistentQueue.Tuple;
 
 class PersistentQueueTest {
+
+  private PersistentQueue empty;
+
+  @BeforeEach
+  void setUp() {
+    empty = new PersistentQueue();
+  }
+
   @Test
   void dequeueFromEmptyQueue() throws Exception {
-    PersistentQueue queue = new PersistentQueue();
+    assertThat(empty.dequeue().value()).isEmpty();
+    assertThat(empty.dequeue().queue()).isEmpty();
+  }
 
-    Tuple ret = queue.dequeue();
+  @Test
+  void enqueueDequeueOneItem() throws Exception {
+    PersistentQueue single = empty.enqueue(1);
 
+    Tuple one = single.dequeue();
+
+    assertThat(one.value()).hasValue(1);
+
+    PersistentQueue prev = one.queue().get();
+
+    Tuple empty = prev.dequeue();
+
+    assertThat(empty.value()).isEmpty();
+  }
+
+  @Test@Disabled
+  void enqueueDequeueManyItems() throws Exception {
+    PersistentQueue queue = empty;
+    for (int i = 1; i < 4; i++) {
+      queue = queue.enqueue(i);
+    }
+
+    Tuple ret;
+
+    ret = queue.dequeue();
+    assertThat(ret.value()).hasValue(1);
+
+    queue = ret.queue().get();
+    ret = queue.dequeue();
+    assertThat(ret.value()).hasValue(2);
+
+    queue = ret.queue().get();
+    ret = queue.dequeue();
+    assertThat(ret.value()).hasValue(3);
+
+    queue = ret.queue().get();
+    ret = queue.dequeue();
     assertThat(ret.value()).isEmpty();
   }
 }
