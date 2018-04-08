13f38d75bdb105419bf303bab0315035ceca087d
Alex-Diez <alex.dukhno@icloud.com>
2018-04-08 11:42:07 +0300

    k max items kata fourth day

---
+++ k-max-items-kata/k-max-items-day04/src/main/java/ua/kata/KMaxItems.java
@@ -0,0 +1,18 @@
+package ua.kata;
+
+public class KMaxItems {
+  public int[] from(int[] numbers, int k) {
+    int[] max = new int[k];
+    System.arraycopy(numbers, 0, max, 0, k);
+    for (int i = k; numbers.length > i; i++) {
+      int j = 0;
+      while (j < k && numbers[i] <= max[j]) {
+        j++;
+      }
+      if (j < k) {
+        max[j] = numbers[i];
+      }
+    }
+    return max;
+  }
+}
