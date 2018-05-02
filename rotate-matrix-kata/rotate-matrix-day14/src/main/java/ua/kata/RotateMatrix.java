906e98e3282fd2a1a0ca65c7050c8d0af3f2d0a9
Alex-Diez <alex.dukhno@icloud.com>
2018-05-02 10:48:45 +0300

    rotate matrix kata fourteenth day

---
+++ rotate-matrix-kata/rotate-matrix-day14/src/main/java/ua/kata/RotateMatrix.java
@@ -0,0 +1,20 @@
+package ua.kata;
+
+public class RotateMatrix {
+  public void rotate(int[][] matrix) {
+    int l = matrix.length - 1;
+    int f = 0;
+
+    while (l > f) {
+      for (int i = 0; i < l - f; i++) {
+        int temp = matrix[l][f + i];
+        matrix[l][f + i] = matrix[l - i][l];
+        matrix[l - i][l] = matrix[f][l - i];
+        matrix[f][l - i] = matrix[f + i][f];
+        matrix[f + i][f] = temp;
+      }
+      l -= 1;
+      f += 1;
+    }
+  }
+}
