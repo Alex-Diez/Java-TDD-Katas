a901a824b5a85e03a1ff33c732c0ed35e071709f
Alex-Diez <alex.dukhno@icloud.com>
2018-05-14 09:55:57 +0300

    shift array left seventh day

---
+++ shift-array-left-kata/shift-array-left-day07/src/main/java/ua/kata/ShiftArrayLeft.java
@@ -0,0 +1,21 @@
+package ua.kata;
+
+public class ShiftArrayLeft {
+  public void shift(int[] items, int k) {
+    int actual = k % items.length;
+    reverse(items, 0, items.length - 1);
+    reverse(items, 0, items.length - actual - 1);
+    reverse(items, items.length - actual, items.length - 1);
+  }
+
+  private void reverse(int[] items, int first, int last) {
+    while (last > first) {
+      int temp = items[first];
+      items[first] = items[last];
+      items[last] = temp;
+
+      last -= 1;
+      first += 1;
+    }
+  }
+}
---
+++ shift-array-left-kata/shift-array-left-day07/src/test/java/ua/kata/ShiftArrayLeftTest.java
@@ -0,0 +1,35 @@
+package ua.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShiftArrayLeftTest {
+  @Test
+  void twoItemsArray_shiftByOne() throws Exception {
+    final int[] items = {1, 2};
+    new ShiftArrayLeft().shift(items, 1);
+    assertThat(items).isEqualTo(new int[]{2, 1});
+  }
+
+  @Test
+  void fourItemsArray_shiftByTwo() throws Exception {
+    final int[] items = {1, 2, 3, 4};
+    new ShiftArrayLeft().shift(items, 2);
+    assertThat(items).isEqualTo(new int[]{3, 4, 1, 2});
+  }
+
+  @Test
+  void fourItemsArray_shiftByOne() throws Exception {
+    final int[] items = {1, 2, 3, 4};
+    new ShiftArrayLeft().shift(items, 1);
+    assertThat(items).isEqualTo(new int[]{2, 3, 4, 1});
+  }
+
+  @Test
+  void fourItemsArray_shiftByFive() throws Exception {
+    final int[] items = {1, 2, 3, 4};
+    new ShiftArrayLeft().shift(items, 5);
+    assertThat(items).isEqualTo(new int[]{2, 3, 4, 1});
+  }
+}
