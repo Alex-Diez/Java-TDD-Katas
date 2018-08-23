5311d41400c2c7aeca3975dd2a2cc4f0dd249c1e
Alex-Diez <alex.dukhno@icloud.com>
2018-08-23 18:46:09 +0300

    leap year kata ninth day

---
+++ leap-year-kata/leap-year-day09/src/test/groovy/ua/kata/LeapYearTest.groovy
@@ -0,0 +1,37 @@
+package ua.kata
+
+import spock.lang.Specification
+
+class LeapYearTest extends Specification {
+  def 'year is leap when divisible by 4'() {
+    given: 'a year that is divisible by 4'
+    def year = new Year(4 * 100 + 4)
+
+    expect: 'the year is a leap year'
+    year.isLeap()
+  }
+
+  def 'year is not leap when not divisible by 4'() {
+    given: 'a year that is not divisible by 4'
+    def year = new Year(4 * 100 + 1)
+
+    expect: 'the year is not a leap year'
+    year.isNotLeap()
+  }
+
+  def 'year is not leap when divisible by 4 and 100'() {
+    given: 'a year that is divisible by 4 and 100'
+    def year = new Year(4 * 100 + 100)
+
+    expect: 'the year is not a leap year'
+    year.isNotLeap()
+  }
+
+  def 'year is leap when divisible by 400'() {
+    given: 'a year that is divisible by 400'
+    def year = new Year(4 * 100)
+
+    expect: 'the year is a leap year'
+    year.isLeap()
+  }
+}
