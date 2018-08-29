cd76dc46c5fba5ff800bec53a233d41f6491d9be
Alex-Diez <alex.dukhno@icloud.com>
2018-08-29 10:26:32 +0300

    leap year kata fourteenth day

---
+++ leap-year-kata/leap-year-day14/src/main/java/ua/kata/Year.java
@@ -0,0 +1,26 @@
+package ua.kata;
+
+public class Year {
+
+  private final int value;
+
+  public Year(int value) {
+    this.value = value;
+  }
+
+  public boolean isLeap() {
+    return (divisibleBy(4) && notDivisibleBy(100)) || divisibleBy(400);
+  }
+
+  private boolean notDivisibleBy(int div) {
+    return !divisibleBy(div);
+  }
+
+  private boolean divisibleBy(int div) {
+    return value % div == 0;
+  }
+
+  public boolean isNotLeap() {
+    return !isLeap();
+  }
+}
